abd3751c1cb74bea721bc5d791e3ba0ae1235b88
Sun Cheng <406504325@qq.com>
2024-05-30 16:22:27 +0800

    fix github build action

---
+++ mpc-backend/apps/templates-engine-api/src/main/kotlin/com/nevermore/mpc/TemplatesEngineApplication.kt
@@ -0,0 +1,15 @@
+package com.nevermore.mpc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+/**
+ * @author nevermore
+ * @since
+ */
+@SpringBootApplication
+class TemplatesEngineApplication
+
+fun main(args: Array<String>) {
+    runApplication<TemplatesEngineApplication>(*args)
+}
