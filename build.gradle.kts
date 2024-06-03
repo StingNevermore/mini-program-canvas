e3d25cb3e985550f7d64dadb91b5629d2dfcca50
Sun Cheng <406504325@qq.com>
2024-06-04 00:20:08 +0800

    redefined clean task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,14 @@
+import java.nio.file.Files
+
 plugins {
     base
+    java
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
 }
 
 fun registerLifecycleTaskDependencies(taskName: String) {
@@ -20,11 +29,45 @@ tasks.register<Copy>("distribution") {
         gradle.includedBuild("mpc-backend").projectDir.resolve("$it/build/libs")
     }
 
-    paths.forEach  {
+    paths.forEach {
         from(files(it)) {
             exclude("*-plain.jar")
         }
         into(layout.buildDirectory.dir("archive/backend-apps"))
     }
     dependsOn("assemble")
+}
+
+val localDockerDevEnvStorageFilesEnsure = tasks.register<Copy>("localDockerDevEnvStorageFilesEnsure") {
+    val mysqlStorageDir = layout.buildDirectory.dir("localStorage/mysql").get().asFile
+    if (!mysqlStorageDir.exists()) {
+        Files.createDirectories(mysqlStorageDir.toPath())
+    }
+}
+
+tasks.register<Exec>("localDockerComposeDevEnv") {
+    executable = "docker"
+    workingDir = projectDir.resolve("dev-tools/docker/compose")
+    args = listOf("compose", "-f", "local-dev-env.yml", "up", "-d")
+    dependsOn(localDockerDevEnvStorageFilesEnsure)
+}
+
+tasks.register<Exec>("shutdownLocalDockerComposeDevEnv") {
+    executable = "docker"
+    workingDir = projectDir.resolve("dev-tools/docker/compose")
+    args = listOf("compose", "-f", "local-dev-env.yml", "down")
+}
+
+tasks.getByName<Delete>("clean") {
+    layout.buildDirectory
+        .asFile
+        .orNull
+        ?.listFiles()
+        ?.filterNot { it.name.equals("localStorage") }
+        ?.toSet()
+        ?.let { delete = it }
+}
+
+tasks.register<Delete>("cleanLocalStorage") {
+    delete = setOf(layout.buildDirectory.dir("localStorage"))
 }
--- dev-tools/docker/init-docker-databases.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-MYSQL=mariadb
-$MYSQL -uroot -p"123456" -h mpc-db < "/init-database.sql"
